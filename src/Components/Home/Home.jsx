3f0bbc6f51b01d61d0bdb03082d04e7f3ad5e55b
mursalin@gmail.com <MURSA>
2023-06-09 00:05:28 +0600

    added popupar section number one

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HomeSlider from "./HomeSlider/HomeSlider";
+import PopularInstructorsSection from "./popular nstructorsSection/popularInstructorsSection";
 
 const Home = () => {
   return (
     <div>
       <div className="">
         <HomeSlider></HomeSlider>
+        <PopularInstructorsSection></PopularInstructorsSection>
       </div>
     </div>
   );
---
+++ src/Components/Home/popular nstructorsSection/popularInstructorsSection.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+
+const PopularInstructorsSection = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  // Sort the data array based on totalClassesTaken in descending order
+  const sortedData = [...data].sort((a, b) => b.totalClassesTaken - a.totalClassesTaken);
+
+  // Slice the sortedData array to show only the top 6 instructors
+  const slicedData = sortedData.slice(0, 6);
+
+  return (
+    <div>
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+          {slicedData.map((instructor, index) => (
+            <div key={index} className="bg-white shadow-md rounded-lg p-4">
+              <img
+                src={instructor.instructorImage}
+                alt={instructor.instructorName}
+                className="w-full h-80 object-cover rounded-lg"
+              />
+              <div className="mt-4">
+                <p className="text-lg font-semibold">{instructor.instructorName}</p>
+                <p className="text-gray-500">Email: {instructor.email}</p>
+                <p className="text-gray-500">Total Classes Taken: {instructor.totalClassesTaken}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopularInstructorsSection;
